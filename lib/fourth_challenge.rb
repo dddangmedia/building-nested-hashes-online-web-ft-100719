05dbed66bd4993bc16299388491d331f726f70e6
Duong Dang <dddangmedia@gmail.com>
2019-10-19 22:17:37 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -39,5 +39,5 @@ def fourth_challenge
  
 end
 
- epic_tragedy[:montague][:hero][:status] = "dead"
-   epic_tragedy[:capulet][:heroine][:status] = "dead"
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
